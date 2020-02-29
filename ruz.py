7114962c8ad9249bfe1ce0edeb606368684bf827
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-02-29 12:52:30 +0300

    Parse base data from ruz

--- ruz.py
+++ ruz.py
@@ -3,8 +3,18 @@ import json
 
 def get_lessons(email, start_date, end_date):
     # response = requests.get(f'http://mlk.hse.ru/mlk-0.5.1/ruz/oldapi/personlessons?fromdate={start_date}&todate={end_date}&email={email}&receivertype=1&language=2')
-    response = requests.get(f'http://mlk.hse.ru/mlk-0.5.1/ruz/api/schedule/student?start={start_date}&finish={end_date}&email={email}&lng=2')
-    return response.content.decode("utf-8")
+    response = requests.get(f'http://mlk.hse.ru/mlk-0.5.1/ruz/api/schedule/student?start={start_date}&finish={end_date}&email={email}&lng=1')
+    resp = json.loads(response.content.decode("utf-8"))
+    # if resp["error"] == "zero-id-request":
+    #     return "No such user"
+    count = len(resp)
+    # print(count)
+    res = f"{count} пар: \n"
+    for i in resp:
+        res+=f"{i['discipline']}\n"
+        #print(i["discipline"])
+    print(res)
+    return res
     #print(response.content.decode("utf-8"))#json.load()
 
-#get_lessons("tvtibilov@edu.hse.ru", "2020.02.29", "2020.02.29")
+get_lessons("tvtibilov@edu.hse.ru", "2020.02.29", "2020.02.29")
