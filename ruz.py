63f6f3bfe24e4ede85f53ae3feddf78047f3e89f
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-02-29 12:58:57 +0300

    Catch wrong email

--- ruz.py
+++ ruz.py
@@ -5,16 +5,15 @@ def get_lessons(email, start_date, end_date):
     # response = requests.get(f'http://mlk.hse.ru/mlk-0.5.1/ruz/oldapi/personlessons?fromdate={start_date}&todate={end_date}&email={email}&receivertype=1&language=2')
     response = requests.get(f'http://mlk.hse.ru/mlk-0.5.1/ruz/api/schedule/student?start={start_date}&finish={end_date}&email={email}&lng=1')
     resp = json.loads(response.content.decode("utf-8"))
-    # if resp["error"] == "zero-id-request":
-    #     return "No such user"
-    count = len(resp)
-    # print(count)
-    res = f"{count} пар: \n"
-    for i in resp:
-        res+=f"{i['discipline']}\n"
-        #print(i["discipline"])
-    print(res)
-    return res
-    #print(response.content.decode("utf-8"))#json.load()
+    try:
+        count = len(resp)
+        res = f"{count} пар: \n"
+        for i in resp:
+            res+=f"{i['discipline']}\n"
+        return res
+    except TypeError as e:
+        return "Не нашла расписания для этого Email"
+    except Exception as e:
+        return "Сервер расписаний не доступен"
 
-get_lessons("tvtibilov@edu.hse.ru", "2020.02.29", "2020.02.29")
+print(get_lessons("tvtibilov@edu.hse.ru", "2020.02.29", "2020.02.29"))
